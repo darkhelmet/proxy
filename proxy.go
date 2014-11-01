400636b81dd0d945669b79aeff2ebd103771ece1
Daniel Huckstep <darkhelmet@darkhelmetlive.com>
2014-10-31 22:56:03 -0600

    simple proxy

---
+++ proxy.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"net/http/httputil"
+	"net/url"
+	"os"
+
+	"github.com/darkhelmet/env"
+)
+
+var (
+	port = env.IntDefault("PORT", 5000)
+)
+
+func main() {
+	proxyUrl, err := url.Parse(env.String("PROXY_URL"))
+	if err != nil {
+		log.Fatalf("failed parsing url: %s", err)
+	}
+	proxy := &httputil.ReverseProxy{
+		Director: func(req *http.Request) {
+			req.URL.Scheme = proxyUrl.Scheme
+			req.URL.Host = proxyUrl.Host
+			req.URL.Path = req.URL.Path
+			req.Host = proxyUrl.Host
+		},
+	}
+	log.Printf("listening on port %d", port)
+	http.ListenAndServe(fmt.Sprintf(":%d", port), proxy)
+}
